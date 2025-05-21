635482cd006e124cabbfb3ed5f9580ba86a7fdf9
AaliyanDevX <aaliyandevx@gmail.com>
2025-05-21 06:17:56 +0500

    starting js

---
+++ scripts/amazon.js
@@ -0,0 +1,9 @@
+const products = [{
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  rating: {
+    stars: 4.5,
+    count: 87,
+  },
+  priceCents: 1090,
+}];
